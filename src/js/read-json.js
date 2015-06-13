8efb56b655b9f6415e4508a9793e2a35e37b9c56
Kohei Arao <koara.local.jp@gmail.com>
2015-06-13 21:44:07 +0900

    update UI

---
+++ src/js/read-json.js
@@ -0,0 +1,50 @@
+var dataPath = "data/";
+var channelName = "general/";
+
+var channelList, channelMassages;
+
+channelList = new Vue({
+    el: '#channelList',
+    data: {
+        channels: []
+    },
+    methods: {
+        onClick: function(e) {
+            channelMassages.channel.name = e.target.innerHTML.slice(1);
+            console.log(channelMassages.channel.name);
+        }
+    },
+    created: function() {
+        $.getJSON(dataPath + "channels.json", function(data) {
+            var len = data.length;
+            for(var i = 0; i < len; i++) {
+                channelList.channels.push(data[i]);
+            }
+        });
+    }
+})
+
+channelMassages = new Vue({
+    el: '#channelMassages',
+    data: {
+        channel: {
+            name: ''
+        },
+        massages: []
+    },
+    watch: {
+        'channel.name' : function() {
+            var path = dataPath + channelMassages.channel.name + "/" + "2015-06-12.json";
+            console.log(path);
+
+            channelMassages.massages = [];
+
+            $.getJSON(path, function(data) {
+                var len = data.length;
+                for(var i = 0; i < len; i++) {
+                    channelMassages.massages.push(data[i]);
+                }
+            });
+        }
+    }
+})
